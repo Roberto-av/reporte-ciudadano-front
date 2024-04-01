7e4139c463dd87447d0419a3da08ffa15a4150ef
Roberto-av <avilesrobertoe@gmail.com>
2024-04-01 15:58:07 -0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,24 +5,26 @@ import "./Assets/style.css";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import ReportsList from "./Pages/ReportsList";
-import User from "./Pages/User";
 import UpdateReport from "./Pages/UpdateReport";
 import AddReport from "./Pages/AddReport";
 import Login from "./Pages/Login";
+import { AuthProvider } from "./Services/AuthContext"; // Importa el proveedor de contexto de autenticación
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/reports" element={<ReportsList />} />
-          <Route path="/reports/update/:reportId" element={<UpdateReport />} />
-          <Route path="/reports/add" element={<AddReport />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <AuthProvider> {/* Envuelve la aplicación con el proveedor de contexto de autenticación */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/reports" element={<ReportsList />} />
+            <Route path="/reports/update/:reportId" element={<UpdateReport />} />
+            <Route path="/reports/add" element={<AddReport />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
