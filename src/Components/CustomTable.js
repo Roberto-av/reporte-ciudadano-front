7a97f140adf05d5a700b4d2e309276c1b026e562
Roberto-av <avilesrobertoe@gmail.com>
2024-04-10 16:01:39 -0700

    Update CustomTable

--- src/Components/CustomTable.js
+++ src/Components/CustomTable.js
@@ -1,7 +1,11 @@
 // CustomTable.js
 import React from 'react';
+import { useAuth } from '../Services/AuthContext';
 
 function CustomTable({ data, columns, actions }) {
+  const { user } = useAuth();
+  const isAdminOrEmployee = user && user.roles && (user.roles.includes("ROLE_ADMIN") || user.roles.includes("ROLE_EMPLOYEE"));
+
   return (
     <table className="table">
       <thead>
@@ -9,18 +13,22 @@ function CustomTable({ data, columns, actions }) {
           {columns.map((column, index) => (
             <th key={index}>{column.header}</th>
           ))}
-          {/* Renderizar una columna adicional para las acciones */}
-          {actions && actions.length > 0 && <th>Acciones</th>}
+          {/* Renderizar una columna adicional para las acciones solo si el usuario es admin o employee */}
+          {isAdminOrEmployee && actions && actions.length > 0 && <th>Acciones</th>}
         </tr>
       </thead>
       <tbody>
         {data.map((row, rowIndex) => (
           <tr key={rowIndex}>
             {columns.map((column, colIndex) => (
-              <td key={colIndex}>{row[column.field]}</td>
+              <td key={colIndex}
+              style={column.style ? column.style(row[column.field]) : {}} 
+              >
+                {row[column.field]}
+              </td>
             ))}
-            {/* Renderizar botones o elementos de acción según las acciones proporcionadas */}
-            {actions && actions.length > 0 && (
+            {/* Renderizar botones o elementos de acción solo si el usuario es admin o employee */}
+            {isAdminOrEmployee && actions && actions.length > 0 && (
               <td>
                 {actions.map((action, actionIndex) => (
                   <button key={actionIndex} onClick={() => action.onClick(row)}>
@@ -32,7 +40,7 @@ function CustomTable({ data, columns, actions }) {
           </tr>
         ))}
       </tbody>
-    </table>
+    </table>  
   );
 }
 
