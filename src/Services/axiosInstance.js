87cb2152c1b5d9dc59ce4481493bfafb936f2f84
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-09 20:16:41 -0700

    Update axiosInstance

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_AUTH_URL,
     headers: {
         'Content-Type': 'application/json',
     },
