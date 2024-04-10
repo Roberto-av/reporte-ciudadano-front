636cf14849b4cbbf0aa19223b340a950b1c8d79c
Roberto-av <avilesrobertoe@gmail.com>
2024-04-10 16:00:57 -0700

    Fixed AuthContext

--- src/Services/AuthContext.js
+++ src/Services/AuthContext.js
@@ -6,31 +6,30 @@ export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token') || null);
-  const [user, setUser] = useState(null); // Agregamos el estado del usuario
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null); // Agregamos el estado del usuario
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true' || false);
 
   const updateToken = (newToken) => {
     setToken(newToken);
     if (newToken) {
-        setIsLoggedIn(true);
-        localStorage.setItem('token', newToken);
+      setIsLoggedIn(true);
+      localStorage.setItem('token', newToken);
     } else {
-        setIsLoggedIn(false);
-        localStorage.removeItem('token');
+      setIsLoggedIn(false);
+      localStorage.removeItem('token');
     }
-};
+  };
 
   const updateUser = ({ id, roles }) => {
-    setUser({
-      id: id,
-      roles: roles
-    });
+    setUser({ id, roles });
+    localStorage.setItem('user', JSON.stringify({ id, roles })); // Almacenamos los roles del usuario
   };
 
   const logout = () => {
     setToken(null);
     localStorage.removeItem('token');
     setUser(null); // Limpiamos el usuario al cerrar sesión
+    localStorage.removeItem('user'); // Limpiamos los roles del usuario al cerrar sesión
     setIsLoggedIn(false);
     localStorage.removeItem('isLoggedIn'); // Removemos el estado de autenticación del almacenamiento local
     console.log('Logout successful');
