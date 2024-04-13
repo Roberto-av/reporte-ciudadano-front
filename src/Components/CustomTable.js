9b2f3e769c98f8dd44fe169dfd977bbb76868082
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-12 19:48:12 -0700

    Update CustomTable

--- src/Components/CustomTable.js
+++ src/Components/CustomTable.js
@@ -2,21 +2,31 @@
 import React from "react";
 import { useAuth } from "../Services/AuthContext";
 
-function CustomTable({ data, columns, actions }) {
+function CustomTable({ data, columns, actions, onSortByStatus  }) {
   const { user } = useAuth();
   const isAdminOrEmployee =
     user &&
     user.roles &&
     (user.roles.includes("ROLE_ADMIN") || user.roles.includes("ROLE_EMPLOYEE"));
 
+    const handleSortByStatus = () => {
+      if (onSortByStatus) {
+        onSortByStatus();
+      }
+    };
+
   return (
     <table className="table">
       <thead>
         <tr>
           {columns.map((column, index) => (
-            <th key={index}>{column.header}</th>
+            <th key={index}>
+            {column.header}
+            {column.field === "status" && (
+              <span style={{cursor: "pointer"}} onClick={handleSortByStatus}> ▼</span>
+            )}
+          </th>
           ))}
-          {/* Renderizar una columna adicional para las acciones solo si el usuario es admin o employee */}
           {isAdminOrEmployee && actions && actions.length > 0 && (
             <th>Acciones</th>
           )}
@@ -36,12 +46,16 @@ function CustomTable({ data, columns, actions }) {
             {/* Renderizar botones o elementos de acción solo si el usuario es admin o employee */}
             {isAdminOrEmployee && actions && actions.length > 0 && (
               <td>
-                {actions.map((action, actionIndex) => (
-                  <button key={actionIndex} onClick={() => action.onClick(row)}>
-                    {action.label}
-                  </button>
-                ))}
-              </td>
+              {actions.map((action, actionIndex) => (
+                <button
+                  key={actionIndex}
+                  onClick={() => action.onClick(row)}
+                  disabled={action.isDisabled && action.isDisabled(row)} // Modificado
+                  >
+                  {action.label}
+                </button>
+              ))}
+            </td>
             )}
           </tr>
         ))}
