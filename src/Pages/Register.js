91b2762ef1acd91bb33db5ce3d9dfdf7c3483325
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-12 19:45:04 -0700

    Add validations and message in register form

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import styles from "../Assets/css/register.module.css";
+import ErrorMessage from "../Components/ErrorMessage";
+import { ReactComponent as Success } from "../Assets/img/success.svg";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,6 +17,26 @@ function Register() {
 
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    // Función para limpiar el mensaje de error después de 10 segundos
+    const timer = setTimeout(() => {
+      setError("");
+    }, 7000);
+    return () => clearTimeout(timer);
+  }, [error]);
+
+  useEffect(() => {
+    // Función para redireccionar después de 3 segundos si el registro fue exitoso
+    let redirectTimer;
+    if (success) {
+      redirectTimer = setTimeout(() => {
+        setRedirect(true);
+      }, 2000);
+    }
+    return () => clearTimeout(redirectTimer);
+  }, [success]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,6 +45,19 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const fields = Object.values(formData);
+    if (fields.some((field) => field === "")) {
+      setError("Todos los campos son obligatorios.");
+      return;
+    }
+
+    // Verificar si la contraseña tiene menos de 8 caracteres
+    if (formData.password.length < 8) {
+      setError("La contraseña debe tener al menos 8 caracteres.");
+      return;
+    }
+
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_API_BASE_AUTH_URL}/auth/register`,
@@ -32,87 +67,108 @@ function Register() {
       setSuccess(true);
     } catch (error) {
       console.error("Registration failed:", error);
-      setError("Error en el registro. Por favor, inténtalo de nuevo.");
+      setError({
+        server: "Error en el registro. Por favor, inténtalo de nuevo.",
+      });
     }
   };
 
-  if (success) {
-    return <Navigate to="/login" />;
-  }
   return (
     <div className={styles.containerRegister}>
       <div className={styles.formContainer}>
-        <h2>REGISTRO</h2>
-        {error && <p class="text-danger">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <div className={styles.inlineFields}>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              placeholder="Nombre"
-            />
-            </div>
-            <div className={styles.inlineFields}>
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              placeholder="Apellidos"
-            />
-            </div>
+        {success ? (
+          <div className={styles.success}>
+            <h2>REGISTRO EXITOSO</h2>
+            <Success className={styles.successIcon} />
+            <p>
+              Tu cuenta ha sido creada con éxito. Serás redirigido al inicio de
+              sesión en unos segundos...
+            </p>
           </div>
-          <div className={styles.formGroup}>
-          <div className={styles.inlineFields}>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Email"
-            />
-            </div>
-            <div className={styles.inlineFields}>
-            <input
-              type="text"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              placeholder="Numero de telefono"
-            />
-            </div>
+        ) : (
+          <div>
+            <h2>REGISTRO</h2>
+            {error && <ErrorMessage message={error} />}
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formGroup}>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    placeholder="Nombre"
+                    pattern="[A-Za-z\s]+"
+                    title="Ingresa solo letras en este campo"
+                  />
+                </div>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    placeholder="Apellidos"
+                    pattern="[A-Za-z\s]+"
+                    title="Ingresa solo letras en este campo"
+                  />
+                </div>
+              </div>
+              <div className={styles.formGroup}>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?"
+                    title="Ingrese un correo electrónico válido"
+                  />
+                </div>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="text"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    placeholder="Numero de telefono"
+                    pattern="[0-9]{10,}"
+                    title="Ingresa un numero de telefono valido"
+                  />
+                </div>
+              </div>
+              <div className={styles.formGroup}>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    placeholder="Nombre de usuario"
+                  />
+                </div>
+                <div className={styles.inlineFields}>
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="Contraseña"
+                  />
+                </div>
+              </div>
+              <div className={styles.formGroup}>
+                <button type="submit">REGISTRARSE</button>
+              </div>
+            </form>
+            <p>
+              ¿Ya tienes una cuenta? <a href="/login">Inicia sesion</a>
+            </p>
           </div>
-          <div className={styles.formGroup}>
-          <div className={styles.inlineFields}>
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              placeholder="Nombre de usuario"
-            />
-            </div>
-            <div className={styles.inlineFields}>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Contraseña"
-            />
-            </div>
-          </div>
-          <div className={styles.formGroup}>
-            <button type="submit">
-              REGISTRARSE
-            </button>
-          </div>
-        </form>
-        <p>¿Ya tienes una cuenta? <a href="/login">Inicia sesion</a></p>
+        )}
       </div>
+      {redirect && <Navigate to="/login" />}
     </div>
   );
 }
