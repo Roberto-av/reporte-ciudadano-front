6201bf3ff985a191b80f1db118ae6e0e4d342024
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-15 03:47:02 -0700

    Add Users in DashBoard

---
+++ src/Pages/dashboard/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../../Assets/css/dashboard.module.css";
+
+function Users() {
+  return (
+    <div>
+      <h1 className={styles.h1custom}>Usuarios</h1>
+    </div>
+  );
+}
+
+export default Users;
